4f838203ab0088b1aa24486af94a8a55f3439c8f
Kaveh <lcaveh@gmail.com>
2021-02-09 11:35:32 -0800

    Create app and add jest,enzyme, style jsx

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p>Main</p>
+      <style jsx>{`
+        p {
+          color: green;
+          margin: 5px;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default App;
